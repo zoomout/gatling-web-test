70e1770f8a39a37f5a59476aaeff92be6c8cb4ff
zoomout <bzastavnyi@gmail.com>
2019-11-30 01:41:57 +0100

    added scenarios for aggregate test and refactored

--- src/test/scala/com/bogdan/GetAggregatedResults.scala
+++ src/test/scala/com/bogdan/GetAggregatedResults.scala
@@ -23,51 +23,27 @@ class GetAggregatedResults extends BaseSimulation {
         jsonPath("$..pricing.C").ofType[Double].gte("0.0"),
         jsonPath("$..pricing.D").ofType[Double].gte("0.0"),
         jsonPath("$..pricing.E").ofType[Double].gte("0.0"),
-        jsonPath("$..track.1").notExists,
-        jsonPath("$..track.2").notExists,
-        jsonPath("$..track.3").notExists,
-        jsonPath("$..track.4").notExists,
-        jsonPath("$..track.5").notExists,
-        jsonPath("$..shipments.1").notExists,
-        jsonPath("$..shipments.2").notExists,
-        jsonPath("$..shipments.3").notExists,
-        jsonPath("$..shipments.4").notExists,
-        jsonPath("$..shipments.5").notExists,
+        jsonPath("$..track").isNull,
+        jsonPath("$..shipments").isNull,
       ))
     .exec(http("get only track")
       .get(session => s"""/aggregation?track=1,2,3,4,5""")
       .check(
         status is 200,
-        jsonPath("$..pricing.A").notExists,
-        jsonPath("$..pricing.B").notExists,
-        jsonPath("$..pricing.C").notExists,
-        jsonPath("$..pricing.D").notExists,
-        jsonPath("$..pricing.E").notExists,
+        jsonPath("$..pricing").isNull,
         jsonPath("$..track.1").exists,
         jsonPath("$..track.2").exists,
         jsonPath("$..track.3").exists,
         jsonPath("$..track.4").exists,
         jsonPath("$..track.5").exists,
-        jsonPath("$..shipments.1").notExists,
-        jsonPath("$..shipments.2").notExists,
-        jsonPath("$..shipments.3").notExists,
-        jsonPath("$..shipments.4").notExists,
-        jsonPath("$..shipments.5").notExists,
+        jsonPath("$..shipments").isNull,
       ))
     .exec(http("get only shipments")
       .get(session => s"""/aggregation?shipments=1,2,3,4,5""")
       .check(
         status is 200,
-        jsonPath("$..pricing.A").notExists,
-        jsonPath("$..pricing.B").notExists,
-        jsonPath("$..pricing.C").notExists,
-        jsonPath("$..pricing.D").notExists,
-        jsonPath("$..pricing.E").notExists,
-        jsonPath("$..track.1").notExists,
-        jsonPath("$..track.2").notExists,
-        jsonPath("$..track.3").notExists,
-        jsonPath("$..track.4").notExists,
-        jsonPath("$..track.5").notExists,
+        jsonPath("$..pricing").isNull,
+        jsonPath("$..track").isNull,
         jsonPath("$..shipments.1[*]").count.is(1),
         jsonPath("$..shipments.2[*]").count.is(2),
         jsonPath("$..shipments.3[*]").count.is(3),
@@ -94,8 +70,32 @@ class GetAggregatedResults extends BaseSimulation {
         jsonPath("$..shipments.4[*]").count.is(4),
         jsonPath("$..shipments.5[*]").count.is(5),
       ))
+    .exec(http("get overlapping partial pricing A,B,C")
+      .get(session => s"""/aggregation?pricing=A,B,C""")
+      .check(
+        status is 200,
+        jsonPath("$..pricing.A").ofType[Double].gte("0.0"),
+        jsonPath("$..pricing.B").ofType[Double].gte("0.0"),
+        jsonPath("$..pricing.C").ofType[Double].gte("0.0"),
+        jsonPath("$..pricing.D").notExists,
+        jsonPath("$..pricing.E").notExists,
+        jsonPath("$..track").isNull,
+        jsonPath("$..shipments").isNull,
+      ))
+    .exec(http("get overlapping partial pricing C,D,E")
+      .get(session => s"""/aggregation?pricing=C,D,E""")
+      .check(
+        status is 200,
+        jsonPath("$..pricing.A").notExists,
+        jsonPath("$..pricing.B").notExists,
+        jsonPath("$..pricing.C").ofType[Double].gte("0.0"),
+        jsonPath("$..pricing.D").ofType[Double].gte("0.0"),
+        jsonPath("$..pricing.E").ofType[Double].gte("0.0"),
+        jsonPath("$..track").isNull,
+        jsonPath("$..shipments").isNull,
+      ))
 
   setUp(scn.inject(
-    rampUsersPerSec(1) to 100 during (10 seconds)
+    rampUsersPerSec(1) to 50 during (10 seconds)
   )).protocols(httpProtocol)
 }
